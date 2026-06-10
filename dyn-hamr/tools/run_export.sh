dfb19b8fa8617ad8b2750d8345beb847dbae2aa0
jufyinf <steinlemon@outlook.com>
2026-06-10 11:46:26 +0800

    modify config files

---
+++ dyn-hamr/tools/run_export.sh
@@ -0,0 +1,7 @@
+python dyn-hamr/tools/export_pose3d_hand.py \
+  --log-dir outputs/logs/video-custom/segment_000_ch1_undistort/segment_000_ch1_undistort-all-shot-0-0--1 \
+  --phase prior \
+  --result outputs/logs/video-custom/segment_000_ch1_undistort/segment_000_ch1_undistort-all-shot-0-0--1/prior/segment_000_ch1_undistort_000000_world_results.npz \
+  --output demo/segment_000_ch1_undistort_prior.pose3d_hand \
+  --mano-output-convention manopth-lr \
+  --fps 29
